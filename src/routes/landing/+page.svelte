28369863b42198bd92f502eed89412ce7f86bc64
ysadamt <adam.teo321@gmail.com>
2024-02-25 00:23:05 -0600

    landing

---
+++ src/routes/landing/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import { Button } from '$lib/components/ui/button';
+</script>
+
+<main class="flex flex-col items-center justify-center w-full h-dvh gap-4">
+	<h1 class="scroll-m-20 text-6xl font-extrabold lg:text-5xl">dash</h1>
+	<p class="scroll-m-20 text-lg lg:text-xl">Drive your car with your phone</p>
+	<Button href="/" class="scroll-m-20">Start Drive</Button>
+</main>
