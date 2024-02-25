fea62eb1d677a1568dee9cf1ef9403ca4e52d8af
NitroGuy10 <naviyer10@gmail.com>
2024-02-25 04:35:28 -0600

    audio!

---
+++ src/lib/scripts/audio.ts
@@ -0,0 +1,18 @@
+import {Howl} from 'howler';
+
+export const alert = new Howl({
+    src: ['/audio/alert.mp3']
+});
+
+export const angry = new Howl({
+    src: ['/audio/angry.mp3']
+});
+
+export const bleep = new Howl({
+    src: ['/audio/bleep.mp3']
+});
+
+export const hooray = new Howl({
+    src: ['/audio/hooray.mp3']
+});
+
---
+++ src/routes/audio/+page.svelte
@@ -0,0 +1,9 @@
+
+<script lang="ts">
+    import { alert, angry, bleep, hooray } from "$lib/scripts/audio";
+</script>
+
+<button on:click={() => {alert.play()}}>Alert</button>
+<button on:click={() => {angry.play()}}>Angry</button>
+<button on:click={() => {bleep.play()}}>Bleep</button>
+<button on:click={() => {hooray.play()}}>Hooray</button>
