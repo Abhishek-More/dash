cbf67dbf28682c483f82154813a42e6fa605c9d8
NitroGuy10 <naviyer10@gmail.com>
2024-02-25 11:28:59 -0600

    same as last time

--- src/routes/audio/+page.svelte
+++ src/routes/audio/+page.svelte
@@ -1,9 +1,9 @@
 
 <script lang="ts">
-    import { alert, angry, bleep, hooray } from "$lib/scripts/audio";
+    import { alertSound, angry, bleep, hooray } from "$lib/scripts/audio";
 </script>
 
-<button on:click={() => {alert.play()}}>Alert</button>
+<button on:click={() => {alertSound.play()}}>Alert</button>
 <button on:click={() => {angry.play()}}>Angry</button>
 <button on:click={() => {bleep.play()}}>Bleep</button>
 <button on:click={() => {hooray.play()}}>Hooray</button>
